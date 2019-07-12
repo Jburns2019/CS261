b6ab2ed1b77c7cb6784231a8bca3f3a56ceb7a17
John Burns <46507384+Jburns2019@users.noreply.github.com>
2019-07-12 11:45:29 -0700

    Add files via upload

---
+++ Assignment_2/CLDeque/circularList.h
@@ -0,0 +1,33 @@
+#ifndef CIRCULAR_LIST_H
+#define CIRCULAR_LIST_H
+
+#ifndef TYPE
+#define TYPE double
+#endif
+
+#ifndef LT
+#define LT(A, B) ((A) < (B))
+#endif
+
+#ifndef EQ
+#define EQ(A, B) ((A) == (B))
+#endif
+
+typedef struct CircularList CircularList;
+
+CircularList* circularListCreate();
+void circularListDestroy(CircularList* list);
+void circularListPrint(CircularList* list);
+void circularListReverse(CircularList* list);
+
+// Deque interface
+
+void circularListAddFront(CircularList* list, TYPE value);
+void circularListAddBack(CircularList* list, TYPE value);
+TYPE circularListFront(CircularList* list);
+TYPE circularListBack(CircularList* list);
+void circularListRemoveFront(CircularList* list);
+void circularListRemoveBack(CircularList* list);
+int circularListIsEmpty(CircularList* list);
+
+#endif
